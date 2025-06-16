dec02348718e4dea7d1aeb2b73532573be10f7ec
Kappanile Kavindu Lakshan Kottegoda <138689980+kavi17498@users.noreply.github.com>
2025-06-16 20:34:23 +0530

    make email as not required

--- lib/screens/client/add_client.dart
+++ lib/screens/client/add_client.dart
@@ -136,14 +136,13 @@ class _AddClientScreenState extends State<AddClientScreen> {
         ),
         style: TextStyle(color: secondaryColor),
         validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Email is required';
-          }
-          // Regex pattern for email validation
-          final emailRegex =
-              RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-          if (!emailRegex.hasMatch(value)) {
-            return 'Enter a valid email address';
+          if (value != null && value.isNotEmpty) {
+            // Regex pattern for email validation
+            final emailRegex =
+                RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+            if (!emailRegex.hasMatch(value)) {
+              return 'Enter a valid email address';
+            }
           }
           return null;
         },
